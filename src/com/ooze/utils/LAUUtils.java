0e01ff663d2c9dd5df536fb068c244d1bf24fe1e
Fabrice <fabrice_75015@yahoo.com>
2024-08-08 18:25:27 +0200

    Fix HMACLAU256

--- src/com/ooze/utils/LAUUtils.java
+++ src/com/ooze/utils/LAUUtils.java
@@ -2,12 +2,15 @@ package com.ooze.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -24,6 +27,10 @@ import org.apache.xml.security.c14n.Canonicalizer;
 import org.w3c.dom.Document;
 
 public class LAUUtils {
+
+    static {
+        org.apache.xml.security.Init.init();
+    }
 
 	// Cipher parameters that are used
 	public static final int AES_KEY_SIZE = 256; // in bits
@@ -45,8 +52,8 @@ public class LAUUtils {
 			StringBuilder hash = new StringBuilder();
 			for(int i = 0; i < bytes.length; i++) {
 				String hex = Integer.toHexString(0xff & bytes[i]);
-                if(hex.length() == 1) hex = "0" + hex;
-                hash.append(hex);
+				if(hex.length() == 1) hex = "0" + hex;
+				hash.append(hex);
 			}
 			returningText = hash.toString().toUpperCase();
 
@@ -60,41 +67,46 @@ public class LAUUtils {
 	// Compute LAU for DataPDU message
 	public static String computeLAU(String dataPDU, String LAU_key) {
 		try {
+			// Step 0 : add the LAU tag
+			dataPDU = dataPDU.replace("</Saa:DataPDU>", "<Saa:LAU></Saa:LAU></Saa:DataPDU>");
+
 			// Step 1 : Convert string to XML document
 			Document doc = convertStringToDocument(dataPDU);
 
-			// Step 2 : Calculate SHA256 of canonized XML
-			byte[] sha256Hash = calculateSHA256(canonizeDocument(doc));
+			// Step 2 : Canonicalize the DataPDU and calculate SHA256 digest
+			byte[] canonicalDataPDU = canonicalizeDocument(doc);
+			byte[] sha256Hash = calculateSHA256(canonicalDataPDU);
 			String sha256base64Encoded = Base64.getEncoder().encodeToString(sha256Hash);
 
 			// Step 3 : Create signedInfo
 			String signedInfo = "<ds:SignedInfo xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\">\n";
-			signedInfo = signedInfo + "<ds:CanonicalizationMethod Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>\n";
-			signedInfo = signedInfo + "<ds:SignatureMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#hmac-sha256\"/>\n";
-			signedInfo = signedInfo + "<ds:Reference URI=\"\">\n";
-			signedInfo = signedInfo + "<ds:Transforms>\n";
-			signedInfo = signedInfo + "<ds:Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#enveloped-signature\"/>\n";
-			signedInfo = signedInfo + "<ds:Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>\n";
-			signedInfo = signedInfo + "</ds:Transforms>\n";
-			signedInfo = signedInfo + "<ds:DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>\n";
-			signedInfo = signedInfo + "<ds:DigestValue>" + sha256base64Encoded + "</ds:DigestValue>\n";
-			signedInfo = signedInfo + "</ds:Reference>\n";
-			signedInfo = signedInfo + "</ds:SignedInfo>\n";
+			signedInfo += "<ds:CanonicalizationMethod Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>\n";
+			signedInfo += "<ds:SignatureMethod Algorithm=\"http://www.w3.org/2001/04/xmldsig-more#hmac-sha256\"/>\n";
+			signedInfo += "<ds:Reference URI=\"\">\n";
+			signedInfo += "<ds:Transforms>\n";
+			signedInfo += "<ds:Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#enveloped-signature\"/>\n";
+			signedInfo += "<ds:Transform Algorithm=\"http://www.w3.org/2001/10/xml-exc-c14n#\"/>\n";
+			signedInfo += "</ds:Transforms>\n";
+			signedInfo += "<ds:DigestMethod Algorithm=\"http://www.w3.org/2001/04/xmlenc#sha256\"/>\n";
+			signedInfo += "<ds:DigestValue>" + sha256base64Encoded + "</ds:DigestValue>\n";
+			signedInfo += "</ds:Reference>\n";
+			signedInfo += "</ds:SignedInfo>\n";
 
-			// Calculate HMAC256 of SignedInfo with secret key
-			Document signedInfoDoc = convertStringToDocument(signedInfo);
-			byte[] canonicalXMLData = canonizeDocument(signedInfoDoc);
-			byte[] secretKey = LAU_key.getBytes(StandardCharsets.UTF_8);
-			byte[] hmacValue = calculateHMACSHA256(canonicalXMLData, secretKey);
+            // Convert signedInfo string to Document for canonicalization
+            Document signedInfoDoc = convertStringToDocument(signedInfo);
+            byte[] canonicalSignedInfo = canonicalizeDocument(signedInfoDoc);
 
-			String hmacBase64 = Base64.getEncoder().encodeToString(hmacValue);
+            // Calculate HMAC-SHA256 of the canonicalized SignedInfo using the full LAU key
+            byte[] secretKey = LAU_key.getBytes(StandardCharsets.UTF_8);
+            byte[] hmacValue = calculateHMACSHA256(canonicalSignedInfo, secretKey);
+            String hmacBase64 = Base64.getEncoder().encodeToString(hmacValue);
 
-			// Step 3 : Add LAU part to the DataPDU
-			dataPDU = dataPDU.substring(0, dataPDU.indexOf("</Saa:DataPDU>"));
-			dataPDU = dataPDU + "<Saa:LAU><ds:Signature xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\">\n";
-			dataPDU = dataPDU + signedInfo;
-			dataPDU = dataPDU + "<ds:SignatureValue>" + hmacBase64 + "</ds:SignatureValue>\n";
-			dataPDU = dataPDU + "</ds:Signature></Saa:LAU></Saa:DataPDU>";
+			// Step 4 : Add LAU part to the DataPDU
+			dataPDU = dataPDU.substring(0, dataPDU.indexOf("<Saa:LAU>"));
+			dataPDU += "<Saa:LAU><ds:Signature xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\">\n";
+			dataPDU += signedInfo.replace("<ds:SignedInfo xmlns:ds=\"http://www.w3.org/2000/09/xmldsig#\">", "<ds:SignedInfo>");
+			dataPDU += "<ds:SignatureValue>" + hmacBase64 + "</ds:SignatureValue>\n";
+			dataPDU += "</ds:Signature></Saa:LAU></Saa:DataPDU>";
 
 			return dataPDU;
 
@@ -120,13 +132,12 @@ public class LAUUtils {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
-		ByteArrayInputStream input = new ByteArrayInputStream(xmlStr.getBytes("UTF-8"));
+		ByteArrayInputStream input = new ByteArrayInputStream(xmlStr.getBytes(StandardCharsets.UTF_8));
 		return builder.parse(input);
 	}
 
-	// Canonize the Document
-	public static byte[] canonizeDocument(Document doc) throws Exception {
-		org.apache.xml.security.Init.init();
+	// Canonicalize the Document
+	public static byte[] canonicalizeDocument(Document doc) throws Exception {
 		Canonicalizer canon = Canonicalizer.getInstance(Canonicalizer.ALGO_ID_C14N_EXCL_OMIT_COMMENTS);
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		canon.canonicalizeSubtree(doc, outputStream);
@@ -174,6 +185,24 @@ public class LAUUtils {
 		// Flush and close the output stream
 		cos.flush();
 		cos.close();
+	}
+
+	// LAU with AES-GCM -> Decrypt beginning of a file to string
+	public static String decryptAESGCMToString(String keyString, File file) throws Exception {
+		try (FileInputStream fis = new FileInputStream(file);
+			 ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+			
+			// Lire seulement les premiers 102400 bytes (ou la taille du fichier si plus petit)
+			byte[] buffer = new byte[102400];
+			int bytesRead = fis.read(buffer);
+			byte[] encryptedData = Arrays.copyOf(buffer, bytesRead);
+
+			try (ByteArrayInputStream bis = new ByteArrayInputStream(encryptedData)) {
+				decryptAESGCM(keyString, bis, bos);
+			}
+
+			return new String(bos.toByteArray(), StandardCharsets.UTF_8);
+		}
 	}
 
 	// LAU with AES-GCM -> Decrypt file
